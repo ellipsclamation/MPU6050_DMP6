a7a0df18993fb784944ca5d0c1731946f8bc4e75
seppc <seppcness@gmail.com>
2017-10-19 19:30:16 -0700

    full

--- MPU6050_DMP6.ino
+++ MPU6050_DMP6.ino
@@ -2,20 +2,60 @@
 #include <ros.h>
 #include <std_msgs/Float32.h>
 
-// I2Cdev and MP6050 Libraries
+// I2Cdev and MP6050 libraries
 #include "I2Cdev.h"
 #include "MPU6050_6Axis_MotionApps20.h"
-#include "Wire.h"
 
+// Arduino Wire library is required if I2Cdev I2CDEV_ARDUINO_WIRE implementation
+// is used in I2Cdev.h
+#if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+    #include "Wire.h"
+#endif
+
+// class default I2C address is 0x68
+// specific I2C addresses may be passed as a parameter here
+// AD0 low = 0x68 (default for SparkFun breakout and InvenSense evaluation board)
+// AD0 high = 0x69
 MPU6050 mpu;
-uint16_t packetSize;
-uint16_t fifoCount;
-uint8_t fifoBuffer[64];
-Quaternion q;
-VectorFloat gravity;
-float ypr[3];
+//MPU6050 mpu(0x69); // <-- use for AD0 hignhh
 
-// Ros setup
+#define OUTPUT_READABLE_YAWPITCHROLL
+
+
+
+#define INTERRUPT_PIN 2  // use pin 2 on Arduino Uno & most boards
+#define LED_PIN 13 // (Arduino is 13, Teensy is 11, Teensy++ is 6)
+bool blinkState = false;
+
+// MPU control/status vars
+bool dmpReady = false;  // set true if DMP init was successful
+uint8_t mpuIntStatus;   // holds actual interrupt status byte from MPU
+uint8_t devStatus;      // return status after each device operation (0 = success, !0 = error)
+uint16_t packetSize;    // expected DMP packet size (default is 42 bytes)
+uint16_t fifoCount;     // count of all bytes currently in FIFO
+uint8_t fifoBuffer[64]; // FIFO storage buffer
+
+// orientation/motion vars
+Quaternion q;           // [w, x, y, z]         quaternion container
+VectorFloat gravity;    // [x, y, z]            gravity vector
+float ypr[3];           // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
+
+
+
+// ================================================================
+// ===               INTERRUPT DETECTION ROUTINE                ===
+// ================================================================
+
+volatile bool mpuInterrupt = false;     // indicates whether MPU interrupt pin has gone high
+void dmpDataReady() {
+    mpuInterrupt = true;
+}
+
+
+// ================================================================
+// ===                      ROS PUBLISHER                       ===
+// ================================================================
+
 ros::NodeHandle nh;
 
 std_msgs::Float32 yaw_msg;
@@ -26,73 +66,167 @@ ros::Publisher chatter_yaw("yaw", &yaw_msg);
 ros::Publisher chatter_pitch("pitch", &pitch_msg);
 ros::Publisher chatter_roll("roll", &roll_msg);
 
-// Setup
+
+// ================================================================
+// ===                      INITIAL SETUP                       ===
+// ================================================================
+
 void setup() {
-  // Advertise Publisher
-  pinMode(13, OUTPUT);
-  nh.initNode();
-  nh.advertise(chatter_yaw);
-  nh.advertise(chatter_pitch);
-  nh.advertise(chatter_roll);
-  
-  Wire.begin();
-  mpu.initialize();
-  mpu.dmpInitialize();
-  mpu.setZAccelOffset(1788);
-  mpu.setXGyroOffset(220);
-  mpu.setYGyroOffset(76);
-  mpu.setZGyroOffset(-85);
-  mpu.setDMPEnabled(true);
-  packetSize = mpu.dmpGetFIFOPacketSize();
-  fifoCount = mpu.getFIFOCount();
-  
-  Serial.begin(115200);
+    // set up for ROS
+    pinMode(13, OUTPUT);
+    nh.initNode();
+    nh.advertise(chatter_yaw);
+    nh.advertise(chatter_pitch);
+    nh.advertise(chatter_roll);
+    
+    // join I2C bus (I2Cdev library doesn't do this automatically)
+    #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+        Wire.begin();
+        Wire.setClock(400000); // 400kHz I2C clock. Comment this line if having compilation difficulties
+    #elif I2CDEV_IMPLEMENTATION == I2CDEV_BUILTIN_FASTWIRE
+        Fastwire::setup(400, true);
+    #endif
+
+    // initialize serial communication
+    // (115200 chosen because it is required for Teapot Demo output, but it's
+    // really up to you depending on your project)
+    Serial.begin(115200);
+    while (!Serial); // wait for Leonardo enumeration, others continue immediately
+
+    // NOTE: 8MHz or slower host processors, like the Teensy @ 3.3V or Arduino
+    // Pro Mini running at 3.3V, cannot handle this baud rate reliably due to
+    // the baud timing being too misaligned with processor ticks. You must use
+    // 38400 or slower in these cases, or use some kind of external separate
+    // crystal solution for the UART timer.
+
+    // initialize device
+    Serial.println(F("Initializing I2C devices..."));
+    mpu.initialize();
+    pinMode(INTERRUPT_PIN, INPUT);
+
+    // verify connection
+    Serial.println(F("Testing device connections..."));
+    Serial.println(mpu.testConnection() ? F("MPU6050 connection successful") : F("MPU6050 connection failed"));
+
+    // load and configure the DMP
+    Serial.println(F("Initializing DMP..."));
+    devStatus = mpu.dmpInitialize();
+
+    // supply your own gyro offsets here, scaled for min sensitivity
+    mpu.setXGyroOffset(220);
+    mpu.setYGyroOffset(76);
+    mpu.setZGyroOffset(-85);
+    mpu.setZAccelOffset(1788); // 1688 factory default for my test chip
+
+    // make sure it worked (returns 0 if so)
+    if (devStatus == 0) {
+        // turn on the DMP, now that it's ready
+        Serial.println(F("Enabling DMP..."));
+        mpu.setDMPEnabled(true);
+
+        // enable Arduino interrupt detection
+        Serial.println(F("Enabling interrupt detection (Arduino external interrupt 0)..."));
+        attachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN), dmpDataReady, RISING);
+        mpuIntStatus = mpu.getIntStatus();
+
+        // set our DMP Ready flag so the main loop() function knows it's okay to use it
+        Serial.println(F("DMP ready! Waiting for first interrupt..."));
+        dmpReady = true;
+
+        // get expected DMP packet size for later comparison
+        packetSize = mpu.dmpGetFIFOPacketSize();
+    } else {
+        // ERROR!
+        // 1 = initial memory load failed
+        // 2 = DMP configuration updates failed
+        // (if it's going to break, usually the code will be 1)
+        Serial.print(F("DMP Initialization failed (code "));
+        Serial.print(devStatus);
+        Serial.println(F(")"));
+    }
+
+    // configure LED for output
+    pinMode(LED_PIN, OUTPUT);
 }
 
-// Main Loop
-void loop() {
-  while (fifoCount < packetSize) {
-    fifoCount = mpu.getFIFOCount();
-  }
-  
-  if (fifoCount == 1024) {
-    mpu.resetFIFO();
-    Serial.println(F("FIFO overflow!"));
-  }
-  else{
-    if (fifoCount % packetSize != 0) {
-      mpu.resetFIFO();
-    }
-    else{
-      while (fifoCount >= packetSize) {
-        mpu.getFIFOBytes(fifoBuffer,packetSize);
-        fifoCount -= packetSize;
-      }
-    
-      mpu.dmpGetQuaternion(&q,fifoBuffer);
-      mpu.dmpGetGravity(&gravity,&q);
-      mpu.dmpGetYawPitchRoll(ypr,&q,&gravity);
-      
-      Serial.print("ypr\t");
-      Serial.print(ypr[0] * 180/M_PI);
-      Serial.print("\t");
-      Serial.print(ypr[1] * 180/M_PI);
-      Serial.print("\t");
-      Serial.print(ypr[2] * 180/M_PI);
-      Serial.println();
-      
-      // Publish message to ROS
-      yaw_msg.data = ypr[0] * 180/M_PI;
-      pitch_msg.data = ypr[1] * 180/M_PI;
-      roll_msg.data = ypr[2] * 180/M_PI;
-      
-      chatter_yaw.publish(&yaw_msg);
-      chatter_pitch.publish(&pitch_msg);
-      chatter_roll.publish(&roll_msg);
-      
-      nh.spinOnce();
-    }
-    
-  }
 
+
+// ================================================================
+// ===                    MAIN PROGRAM LOOP                     ===
+// ================================================================
+
+void loop() {
+    // if programming failed, don't try to do anything
+    if (!dmpReady) return;
+
+    // wait for MPU interrupt or extra packet(s) available
+    while (!mpuInterrupt && fifoCount < packetSize) {
+        // other program behavior stuff here
+        // .
+        // .
+        // .
+        // if you are really paranoid you can frequently test in between other
+        // stuff to see if mpuInterrupt is true, and if so, "break;" from the
+        // while() loop to immediately process the MPU data
+        // .
+        // .
+        // .
+    }
+
+    // reset interrupt flag and get INT_STATUS byte
+    mpuInterrupt = false;
+    mpuIntStatus = mpu.getIntStatus();
+
+    // get current FIFO count
+    fifoCount = mpu.getFIFOCount();
+
+    // check for overflow (this should never happen unless our code is too inefficient)
+    if ((mpuIntStatus & 0x10) || fifoCount == 1024) {
+        // reset so we can continue cleanly
+        mpu.resetFIFO();
+        Serial.println(F("FIFO overflow!"));
+
+    // otherwise, check for DMP data ready interrupt (this should happen frequently)
+    } else if (mpuIntStatus & 0x02) {
+        // wait for correct available data length, should be a VERY short wait
+        while (fifoCount < packetSize) fifoCount = mpu.getFIFOCount();
+
+        // read a packet from FIFO
+        mpu.getFIFOBytes(fifoBuffer, packetSize);
+        
+        // track FIFO count here in case there is > 1 packet available
+        // (this lets us immediately read more without waiting for an interrupt)
+        fifoCount -= packetSize;
+
+        #ifdef OUTPUT_READABLE_YAWPITCHROLL
+            // display Euler angles in degrees
+            mpu.dmpGetQuaternion(&q, fifoBuffer);
+            mpu.dmpGetGravity(&gravity, &q);
+            mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
+
+//            Serial.print("ypr\t");
+//            Serial.print(ypr[0] * 180/M_PI);
+//            Serial.print("\t");
+//            Serial.print(ypr[1] * 180/M_PI);
+//            Serial.print("\t");
+//            Serial.println(ypr[2] * 180/M_PI);
+
+            // Publish message to ROS
+            yaw_msg.data = ypr[0] * 180/M_PI;
+            pitch_msg.data = ypr[1] * 180/M_PI;
+            roll_msg.data = ypr[2] * 180/M_PI;
+            
+            chatter_yaw.publish(&yaw_msg);
+            chatter_pitch.publish(&pitch_msg);
+            chatter_roll.publish(&roll_msg);
+            
+            nh.spinOnce();
+            delay(10);
+        #endif
+
+
+        // blink LED to indicate activity
+        blinkState = !blinkState;
+        digitalWrite(LED_PIN, blinkState);
+    }
 }
